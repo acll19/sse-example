65d2234aae510cadc7e44bedfe4d37cb2526f367
Armando <acll19@gmail.com>
2023-02-26 20:46:01 +0100

    uses completablefuture and allows reconnects

--- src/main/java/org/aca/resources/SseProcessResource.java
+++ src/main/java/org/aca/resources/SseProcessResource.java
@@ -1,6 +1,7 @@
 package org.aca.resources;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -14,13 +15,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.sse.OutboundSseEvent;
 import javax.ws.rs.sse.Sse;
-import javax.ws.rs.sse.SseBroadcaster;
 import javax.ws.rs.sse.SseEventSink;
 
 import org.aca.scheduler.CounterBean;
-
-import io.smallrye.mutiny.Uni;
-import io.smallrye.mutiny.infrastructure.Infrastructure;
 
 @Path("/process")
 @ApplicationScoped
@@ -29,7 +26,8 @@ public class SseProcessResource {
     private Logger logger = Logger.getLogger(SseProcessResource.class.getName());
 
     private Sse sse;
-    private SseBroadcaster broadcaster;
+    private SseEventSink sseEventSink = null;
+    private boolean started = false;
 
     @Inject
     CounterBean counter;
@@ -38,13 +36,9 @@ public class SseProcessResource {
     @Path("/start")
     @Produces(MediaType.APPLICATION_JSON)
     public Response start() {
-        if (counter.get() == 0) {
-            // Do not wait for process to end
-            Uni.createFrom()
-                    .nullItem()
-                    .emitOn(Infrastructure.getDefaultWorkerPool())
-                    .subscribe()
-                    .with(item -> process(), Throwable::printStackTrace);
+        if (!started) {
+            CompletableFuture.runAsync(() -> process());
+            started = true;
             return Response.ok(String.format("{\"status\":\"started\", \"progress\":%d}", 0)).build();
         }
         return Response.ok(String.format("{\"status\":\"in progress\", \"progress\":%d}", counter.get())).build();
@@ -53,31 +47,27 @@ public class SseProcessResource {
     @GET
     @Path("/status")
     @Produces(MediaType.SERVER_SENT_EVENTS)
-    public void subscribeToProcessStatus(@Context SseEventSink sink, @Context Sse sse) {
-        if (this.sse == null || this.broadcaster == null) {
-            this.sse = sse;
-            this.broadcaster = sse.newBroadcaster();
-        }
-
-        this.broadcaster.register(sink);
-        logger.info("New sink registered to broadcaster.");
+    public void status(@Context SseEventSink sink, @Context Sse sse) {
+        this.sse = sse;
+        this.sseEventSink = sink;
     }
 
     private void process() {
         while (counter.get() < 100) {
-            counter.increment();
-            sleep();
-            if (counter.get() < 100) {
+            this.counter.increment();
+            this.sleep();
+            if (this.counter.get() < 100) {
                 // The event name has to be message by default so the onmessage works on the
                 // client
                 // otherwise the client needs to use the addEventListener function to subscribe
                 // to different events
-                broadcastData("message",
+                this.sendProgressEvent("message",
                         String.format("{\"status\":\"in progress\", \"progress\":%d}", counter.get()));
             }
         }
-        if (counter.get() == 100) {
-            broadcastData("message", String.format("{\"status\":\"done\", \"progress\":%d}", counter.get()));
+        if (this.counter.get() == 100) {
+            sendProgressEvent("message", String.format("{\"status\":\"done\", \"progress\":%d}", counter.get()));
+            this.started = false;
         }
     }
 
@@ -89,14 +79,15 @@ public class SseProcessResource {
         }
     }
 
-    private void broadcastData(String name, Object data) {
-        if (broadcaster != null) {
+    private void sendProgressEvent(String name, Object data) {
+        if (this.sseEventSink != null && this.sse != null) {
             OutboundSseEvent event = sse.newEventBuilder()
                     .name(name)
                     .data(data.getClass(), data)
                     .mediaType(MediaType.APPLICATION_JSON_TYPE)
+                    .reconnectDelay(3000)
                     .build();
-            broadcaster.broadcast(event);
+            this.sseEventSink.send(event);
         } else {
             logger.info("Unable to send SSE. Broadcaster context is not set up.");
         }
